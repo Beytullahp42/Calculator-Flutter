161bebb64d7c27ee763c4c6203f3271d5a667221
Beytullahp42 <beytullahpay42@gmail.com>
2025-02-23 16:35:52 +0200

    hey

---
+++ android/app/src/main/kotlin/com/example/calculator_bp/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.calculator_bp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,120 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Calculator'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  String placeholder = "This text is a placeholder.";
+
+  @override
+  Widget build(BuildContext context) {
+    return app();
+  }
+
+  Widget app() {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text(widget.title),
+        ),
+        body: Column(
+          children: [
+            Expanded(
+              child: Container(
+                alignment: Alignment.bottomRight,
+                padding: EdgeInsets.only(right: 16, bottom: 8, left: 16),
+                child: Text(
+                  placeholder,
+                  style: TextStyle(fontSize: 48),
+                ),
+              ),
+            ),
+            Divider(
+              height: 1,
+              color: Colors.green,
+              thickness: 1,
+            ),
+            Container(
+              child: buttons(),
+            ),
+          ],
+        ));
+  }
+
+  Widget buttons() {
+    return GridView.count(
+      crossAxisCount: 4,
+      childAspectRatio: 1.25,
+      shrinkWrap: true,
+      physics: NeverScrollableScrollPhysics(),
+      children: List.generate(buttonList.length, (index) {
+        return calculatorButtons(buttonList[index]);
+      }),
+    );
+  }
+
+  Widget calculatorButtons(String text) {
+    Color color = Colors.black;
+    Color backgroundColor = Colors.transparent;
+    if (text == "=") {
+      color = Colors.white;
+      backgroundColor = Colors.green;
+    } else if (text == "C" ||
+        text == "!" ||
+        text == "mod" ||
+        text == "÷" ||
+        text == "×" ||
+        text == "–" ||
+        text == "+" ||
+        text == "⌫") {
+      color = Colors.green;
+    }
+    return TextButton(
+      style: TextButton.styleFrom(
+        backgroundColor: backgroundColor,
+      ),
+      onPressed: () {
+        // Just for the UI showcase, the logic is not implemented yet.
+        setState(() {
+          placeholder = text;
+        });
+      },
+      child: Text(text, style: TextStyle(color: color, fontSize: 24)),
+    );
+  }
+
+  List<String> buttonList = [
+    "C", "!", "mod", "÷",
+    "7", "8", "9", "×",
+    "4", "5", "6", "–",
+    "1", "2", "3", "+",
+    "⌫", "0", ".", "="
+  ];
+}
