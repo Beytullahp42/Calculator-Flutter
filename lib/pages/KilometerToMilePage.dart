123a8b0d96dd15d37c3f0f7aa37ecdf07955ac96
Beytullahp42 <beytullahpay42@gmail.com>
2025-03-03 16:32:34 +0200

    logic

--- lib/pages/KilometerToMilePage.dart
+++ lib/pages/KilometerToMilePage.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import 'Page.dart';
 
 class KilometerToMilePage extends StatefulWidget {
@@ -12,6 +11,7 @@ class KilometerToMilePage extends StatefulWidget {
 }
 
 class _KilometerToMilePageState extends State<KilometerToMilePage> {
+
   @override
   Widget build(BuildContext context) {
     return app();
@@ -19,14 +19,55 @@ class _KilometerToMilePageState extends State<KilometerToMilePage> {
 
   Widget app() {
     return MainPage(
-        title: "Kilometer to Mile Converter", body: body(), context: context
+      title: "Kilometer to Mile Converter",
+      body: body(),
+      context: context,
     );
+  }
+
+  TextEditingController mileToKmController = TextEditingController();
+  TextEditingController kmToMileController = TextEditingController();
+
+  void convertKmToMile(String input) {
+    double km = double.tryParse(input) ?? 0;
+    double mile = km * 0.621371;
+    mileToKmController.text = mile.toString();
+  }
+
+  void convertMileToKm(String input) {
+    double mile = double.tryParse(input) ?? 0;
+    double km = mile / 0.621371;
+    kmToMileController.text = km.toString();
   }
 
   Column body() {
     return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        Text("Kilometer to Mile Converter"),
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: TextField(
+            controller: kmToMileController,
+            keyboardType: TextInputType.number,
+            decoration: const InputDecoration(
+              labelText: "Kilometers",
+              border: OutlineInputBorder(),
+            ),
+            onChanged: convertKmToMile,
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: TextField(
+            controller: mileToKmController,
+            keyboardType: TextInputType.number,
+            decoration: const InputDecoration(
+              labelText: "Miles",
+              border: OutlineInputBorder(),
+            ),
+            onChanged: convertMileToKm,
+          ),
+        ),
       ],
     );
   }
